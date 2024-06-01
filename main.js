b641c03b242f5f9652f9a2895a31819a091eca08
Bashar <bashar.khdr.ds.92@hotmail.com>
2024-06-02 00:41:14 +0200

    Added id to each task

--- main.js
+++ main.js
@@ -24,28 +24,45 @@ function formatDate(date) {
     return `${day} ${monthsDict[month.toString()]}, ${_2digitYear}`
 }
 
-function appendNewTask (taskText, formattedDate, items) {
+function appendNewTask (task, items, isNewTask=true) {
+
     var newTask = document.createElement('li');
     newTask.classList.add('list-item');
     newTask.classList.add('card')
     const title = document.createElement('h3');
     title.classList.add('task-title');
-    title.innerText = taskText;
+    title.innerText = task.title;
     const deadlineEl = document.createElement('small');
+    const idField = document.createElement('input');
+    idField.type = 'hidden';
+    idField.value = task.id;
     deadlineEl.classList.add('deadline');
-    deadlineEl.innerText = `Must be accomplished before ${formattedDate}`;
+    deadlineEl.innerText = `Must be accomplished before ${task.deadline}`;
+
+    newTask.appendChild(idField);
     newTask.appendChild(title);
     newTask.appendChild(deadlineEl);
     items.appendChild(newTask);
 }
 
-function setMinDate(dateInput) {
+function setMinDate() {
+    var deadlineInput = document.getElementById('task-deadline');
     var minDate = new Date(Date.now());
 
     const year = minDate.getFullYear(); // returns a 4-digit year
-    const month = minDate.getMonth() + 1; // get the month / 0-indexed / + 1
-    const day = minDate.getDate(); // get the day of the month
-    dateInput.min = `${year}-0${month}-${day}`;
+    let month = minDate.getMonth() + 1; // get the month / 0-indexed / + 1
+    let day = minDate.getDate(); // get the day of the month
+    if (month < 10) {
+        month = `0${month}`
+    }
+
+    if (day < 10 ) {
+        day = `0${day}`
+    }
+    deadlineInput.min = `${year}-${month}-${day}`;
+
+    delete deadlineInput;
+    delete minDate;
 }
 
 function clearTasks () {
@@ -55,32 +72,40 @@ function clearTasks () {
 function submitInput() {
 
     var items = document.getElementById('tasks-list')
+    var deadlineInput = document.getElementById('task-deadline');
 
     var taskInput = document.getElementById('task-text');
     var taskText = taskInput.value;
-    var deadline = deadlineInput.value;
+    var taskDeadline = deadlineInput.value;
     
-    const formattedDate = formatDate(deadline);
+    const formattedDate = formatDate(taskDeadline);
     var errorMessage = document.getElementsByClassName('error-message')[0];
     if (taskText === null || taskText === '') {
         taskInput.classList.add('task-text-error');
         errorMessage.style.display = 'block';
     } else {
-        appendNewTask(taskText, formattedDate, items);
+        appendNewTask(
+            {id: generateId().toString(),title: taskText, deadline: formattedDate}, items);
         errorMessage.style.display = 'none';
         taskInput.classList.remove('task-text-error');
     }
+}
+
+function generateId() {
+    return Math.floor(Math.random() * 100000001)
 }
 
 window.addEventListener('beforeunload', function() {
     var tasks = [];
     var items = document.getElementsByTagName('li')
     for (let item of items) {
+        var id = item.getElementsByTagName('input')[0].value;
         var title = item.getElementsByClassName('task-title')[0].innerText;
         var deadline = item.getElementsByClassName('deadline')[0]
             .innerText
             .replace('Must be accomplished before ', '');
         tasks.push({
+            id,
             title,
             deadline
         })
@@ -91,17 +116,16 @@ window.addEventListener('beforeunload', function() {
 
 window.addEventListener('load', function() {
 
+    setMinDate();
+
     var tasks = JSON.parse(this.localStorage.getItem('tasks'));
 
     if (tasks.length > 0) {
         var items = this.document.getElementById('tasks-list');
         for (let task of tasks) {
-            appendNewTask(task.title, task.deadline, items)
+            appendNewTask(task, items, isNewTask = false)
         }
     }
-
-    var deadlineInput = document.getElementById('task-deadline');
-    setMinDate(deadlineInput)
     
     var buttonSubmit = document.getElementsByTagName('button')[0];
     var clearButton = document.getElementsByTagName('button')[1];
